ae2b9e51d926b43bb551ccd442814392b9589f7a
netsap <lucas@Mint.local>
2020-02-19 15:32:17 +0000

    Scraper gathers links correctly

--- Scraper.py
+++ Scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from time import sleep
 import re
 
 
@@ -10,7 +11,7 @@ def page_scraper():
     count = 0
     unformatted_urls = []
 
-    while count < 20:
+    while count < 10:
         pageURL = 'https://www.yelp.co.uk/search?find_desc=&find_loc=Leeds%2C%20West%20Yorkshire&start='+ str(count)
         
         page = urlopen(pageURL)
@@ -22,11 +23,10 @@ def page_scraper():
             if '?' in item.attrs['href']:
                 continue
             else:
-                #print (item.attrs['href'])
                 unformatted_urls.append(item.attrs['href'])
-                #print (unformatted_urls)
         
         count += 10
+        sleep(5)
     else:
         format_unformatted_urls(unformatted_urls)
         print ('exiting...')
@@ -36,14 +36,10 @@ def format_unformatted_urls(unformatted_urls):
     urls = []
     unformatted_urls = list (dict.fromkeys(unformatted_urls))
     for url in unformatted_urls:
-        url = 'https://yelp.co.uk'.join(url)
-        print (url)
-        #urls.append(url)
+        url = 'https://yelp.com%s'%url
+        urls.append(url)
+
     print (urls)
-    print ('length' + len(urls))
-
-
-    print ('Length' + len(unformatted_urls))
 
 
 def site_scraper(url):
@@ -53,6 +49,5 @@ def site_scraper(url):
     test3 = soup.select('a[href*=biz_redir]')
     for item in test3:
         print(item.get_text())
-
 
 page_scraper()
