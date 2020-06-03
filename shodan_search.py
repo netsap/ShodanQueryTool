eb87f2efb0336ca9b4954fb9d9a9835180dd11e5
Netsap <lucas@netsap.xyz>
2020-06-03 22:27:29 +0100

    Added check_vulns, host compatibility and refactoring

--- shodan_search.py
+++ shodan_search.py
@@ -1,5 +1,6 @@
 from database import check_org, check_host, check_service,\
-     query_input, log_no_vulns, insert_new_vulns, write_log_file
+    query_input, log_no_vulns, check_vulns,\
+    write_log_file, log_no_shodan_id, host_search_query
 import shodan
 from time import time, sleep
 from datetime import datetime
@@ -18,14 +19,15 @@ def file_parser(file_path='ShodanQueries'):
 
 
 def search(query):
-    page_number = 1
-    total_pages = 2
     first_run = True
+    page_number = 0
+    total_pages = 1
     print(f'Searching Shodan for {query}')
     while page_number < total_pages:
         try:
-            results = api.search(query, page=page_number, limit=None)
-            sleep(2)
+            results = api.search(
+                query, page=page_number, limit=None, minify=False)
+            sleep(1)
             if first_run is True:
                 total_results = results['total']
                 total_pages = int(total_results / 100 + 1)
@@ -34,33 +36,76 @@ def search(query):
                 print(f'Searching Page: {page_number}/{total_pages}')
             page_number += 1
             for result in results['matches']:
-                parse_results(result)
+                sort_results(result)
         except shodan.exception.APIError as e:
-            print(f'Timeout, Slowing down request rate')
-            sleep(2)
+            print(f'Timeout, pausing requests for 1 second')
+            sleep(1)
         except KeyError as e:
-            print(f'No response, Slowing down request rate')
-            sleep(2)
+            print(f'No response, pausing requests for 1 second')
+            sleep(1)
     if write_log_file.called is True:
         print('Entries added to log.txt')
 
 
-def parse_results(result):
+def search_hosts():
+    hosts = host_search_query()
+    for row in hosts:
+        ip_str = row.ip_str
+        org_id = row.organisation_id
+        host_id = row.id
+        try:
+            host_search_result = api.host("194.187.248.111", minify=False)
+            sort_results(host_search_result, host_search=True)
+        except shodan.exception.APIError:
+            sleep(1)
+            print("Timeout")
+
+
+def sort_results(result, host_search=False):
     org = result.get("org", "n/a")
     org_id = check_org(org)
     ip_str = result["ip_str"]
     asn = result.get("asn", "n/a")
+    if not host_search:
+        parse_search_results(result, org, org_id, ip_str, asn)
+    else:
+        parse_host_results(result, org, org_id, ip_str, asn)
+
+
+def parse_search_results(result, org, org_id, ip_str, asn):
     resultLocation = result['location']
     city = resultLocation.get("city", "n/a")
     country_code = resultLocation.get("country_code", "n/a")
     timestamp = result["timestamp"]
+    host_id = check_host(ip_str, asn, city, country_code, org_id)
+    shodan_module, port, transport, product, vendor_id, data,\
+        shodan_id, vulns, domain, hostname = data_extraction(result)
+    pass_data_to_database(
+        shodan_module, port, transport, product, hostname, domain,
+        data, timestamp, shodan_id, vulns, vendor_id, org, org_id, host_id)
+
+
+def parse_host_results(result, org, org_id, ip_str, asn):
+    city = result['city']
+    country_code = result['country_code']
+    timestamp = result['last_update']
+    host_id = check_host(ip_str, asn, city, country_code, org_id)
+    for row in result['data']:
+        shodan_module, port, transport, product, vendor_id, data,\
+            shodan_id, vulns, domain, hostname = data_extraction(row)
+        pass_data_to_database(
+            shodan_module, port, transport, product, hostname, domain,
+            data, timestamp, shodan_id, vulns, vendor_id, org, org_id, host_id)
+
+
+def data_extraction(result):
+    shodan_meta = result.get("_shodan")
+    shodan_module = shodan_meta.get("module", "n/a")
     port = result.get("port", 0)
     transport = result.get("transport", "n/a")
     product = result.get("product", "n/a")
     vendor_id = result.get("vendor_id", "n/a")
     data = result.get("data", "")
-    shodan_meta = result.get("_shodan")
-    shodan_module = shodan_meta.get("module", "n/a")
     shodan_id = shodan_meta.get("id")
     vulns = result.get("vulns", None)
     try:
@@ -77,21 +122,32 @@ def parse_results(result):
         hostname = "n/a"
     # https://github.com/cudeso/tools/blob/master/shodan-asset-monitor/shodan-asset-monitor.py
     # Clean up unwanted strings from product and vendor
-    product = product.strip("/\n,/\r").replace("&nbsp;", " ").strip()
-    vendor_id = vendor_id.strip("/\n,/\r").replace("&nbsp;", " ").strip()
+    product = product.strip(
+        "/\n,/\r").replace("&nbsp;", " ").strip()
+    vendor_id = vendor_id.strip(
+        "/\n,/\r").replace("&nbsp;", " ").strip()
+    return shodan_module, port, transport, product, vendor_id, data,\
+        shodan_id, vulns, domain, hostname
 
-    host_id = check_host(ip_str, asn, city, country_code, org_id)
+
+def pass_data_to_database(
+        shodan_module, port, transport, product, hostname, domain,
+        data, timestamp, shodan_id, vulns, vendor_id, org, org_id, host_id):
     if shodan_id is None:
-        service_id = log_no_shodan_id(
-          port, transport, product, org, org_id, host_id,
-          vendor_id, shodan_module, vulns)
+        service_id = log_no_shodan_id
+        (port, transport, product, org, org_id, host_id,
+            vendor_id, shodan_module, vulns)
     else:
         service_id = check_service(
-          port, transport, product, shodan_module, hostname, domain, data,
-          timestamp, shodan_id, vendor_id,   org_id, host_id, org)
+            shodan_module, port, transport, product, hostname,
+            domain, data, timestamp, shodan_id, vendor_id,
+            org, org_id, host_id)
     if vulns:
-        cve, cvss, summary, reference, verified = parse_results(vulns)
-        insert_new_vulns(cve, cvss, summary, reference, verified)
+        for vuln in vulns:
+            cve, cvss, summary, reference, verified = parse_vulns(vulns)
+            check_vulns(
+                cve, cvss, summary, reference, verified,
+                org_id, host_id, service_id)
     else:
         log_no_vulns(service_id)
 
@@ -107,7 +163,7 @@ def parse_vulns(vulns):
             verified = 0
         elif veri is True:
             verified = 1
-    return cve, cvss, summary, reference, verified
+        return cve, cvss, summary, reference, verified
 
 
 if __name__ == '__main__':
